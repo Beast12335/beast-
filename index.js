e3f860d3adb73bc70cac4032d8c796a7d161a5ac
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2022-10-13 14:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ return lib.discord.channels['@0.3.2'].messages.create({
     if (message.channelId == '1028321836666200185') {
       console.log(message)
       var clan = await beast.getClan(message.content)
-      var attacks = await clan.clan.attacks			
+      // var attacks = await clan.clan.attacks			
       beast.events.addWars(['message.content']);
       for (let i=0;i<clan.members.length;i++) {
         let b = await lib.mysql.db['@0.2.1'].query({
@@ -56,6 +56,37 @@ beast.events.setWarEvent({
   filter: (oldWar, newWar) => {
     return oldWar.clan.attackCount !== newWar.clan.attackCount; 
     }
+});
+beast.on('attackChange',async(oldWar, newWar) => {
+  console.log(oldWar.clan.attackCount, newWar.clan.attackCount);
+  if (attacks[attacks.length-1].stars === '3'){
+    await lib.mysql.db['@0.2.1'].query({
+      query: `update players set three = ${three+1} where tag = ${attacks[attacks.length-1].attackerTag};`,
+      charset: `UTF8MB4`
+});
+}
+  else if (attacks[attacks.length-1].stars === '2'){
+    await lib.mysql.db['@0.2.1'].query({
+      query: `update players set two = ${two+1} where tag = ${attacks[attacks.length-1].attackerTag};`,
+      charset: `UTF8MB4`
+});
+  }
+  else if (attacks[attacks.length-1].stars === '1'){
+    await lib.mysql.db['@0.2.1'].query({
+      query: `update players set one = ${one+1} where tag = ${attacks[attacks.length-1].attackerTag};`,
+      charset: `UTF8MB4`
+});
+    }
+  else if (attacks[attacks.length-1].stars === '0'){
+    await lib.mysql.db['@0.2.1'].query({
+      query: `update players set zero = ${zero+1} where tag = ${attacks[attacks.length-1].attackerTag};`,
+      charset: `UTF8MB4`
+});
+    }
+  return lib.discord.channels['@0.3.2'].messages.create({
+    channel_id: `860512303233236995`,
+    content: `<@849123406477656086>`
+})
 });
 (async function () {
 	await beast.login({email:process.env.mail,password:process.env.pass})
