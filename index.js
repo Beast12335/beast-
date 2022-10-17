79b6d1ab71a3e5907a69f4f1a2a4bcf7c46a3e04
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2022-10-17 21:39:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ return lib.discord.channels['@0.3.2'].messages.create({
 })
 });
 let clans = [];
-async function () {
+(async function () {
   client.on('messageCreate', async(message) => {
     if (message.channelId == '1028321836666200185') {
       console.log(message)
