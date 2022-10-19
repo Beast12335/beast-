7c3aa1fec11b071482b5ac214795ff94bd3394ad
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2022-10-19 21:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,12 +68,12 @@ return lib.discord.channels['@0.3.2'].messages.create({
         //  query: `update master set state = 'inWar' where clan = ${a.result[i].clan};`,
          // charset: `UTF8MB4`
      //   });   }
-     // else if (state.state === 'inWar') {
-        //await lib.mysql.db['@0.2.1'].query({
-        //  query: `update master set state = 'warEnded' where clan = ${a.result[i].clan};`,
-        //  charset: `UTF8MB4`
-      //  });  }
-      if (state.state === 'warEnded') {
+     if (state.state === 'inWar') {
+        await lib.mysql.db['@0.2.1'].query({
+          query: `update master set state = 'warEnded' where clan = ${a.result[i].clan};`,
+          charset: `UTF8MB4`
+        });  }
+      else if (state.state === 'warEnded') {
         await lib.mysql.db['@0.2.1'].query({
           query: `update master set state = 'notInWar' where clan = ${a.result[i].clan};`,
           charset: `UTF8MB4`
